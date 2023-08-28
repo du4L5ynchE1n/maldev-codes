3e424f7030f42ab6c347b75225ad79e38ee30ff1
e1n <69242462+du4L5ynchE1n@users.noreply.github.com>
2023-08-28 22:22:28 +0800

    Create LoadDLL.c

---
+++ basics/DLL_loading/LoadDLL.c
@@ -0,0 +1,28 @@
+#include <Windows.h>
+#include <stdio.h>
+
+typedef void (WINAPI* HelloUserFunctionPointer)();
+
+int main() {
+    // Load the DLL
+    HMODULE hModule = LoadLibraryA("C:\\Users\\Administrator\\source\\repos\\MalDevDll\\x64\\Debug\\MalDevDll.dll");
+    if (hModule == NULL) {
+        printf("Failed to load DLL. Error code: %d\n", GetLastError());
+        return 1;
+    }
+
+    // Get a function pointer to the exported function
+    HelloUserFunctionPointer helloUser = (HelloUserFunctionPointer)GetProcAddress(hModule, "HelloUser");
+    if (helloUser != NULL) {
+        // Call the exported function
+        helloUser();
+    }
+    else {
+        printf("Failed to locate function. Error code: %d\n", GetLastError());
+    }
+
+    // Unload the DLL
+    FreeLibrary(hModule);
+
+    return 0;
+}
