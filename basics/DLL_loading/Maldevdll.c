4911b4c9b4a2992e566d374d0a1c1b8674b3c29a
e1n <69242462+du4L5ynchE1n@users.noreply.github.com>
2023-08-28 22:24:59 +0800

    Create Maldevdll.c

---
+++ basics/DLL_loading/Maldevdll.c
@@ -0,0 +1,39 @@
+// dllmain.c : Defines the entry point for the DLL application.
+#include <Windows.h>
+#include <stdio.h>
+
+extern __declspec(dllexport) void HelloUser() {
+    char buffer[256];
+    DWORD bufferSize = sizeof(buffer);
+
+    if (GetUserNameA(buffer, &bufferSize)) {
+        char message[256];
+        sprintf_s(message, sizeof(message), "Hello, %s!", buffer);
+        MessageBoxA(NULL, message, "Username Message", MB_OK);
+    }
+    else {
+        printf("Failed to retrieve username. Error code: %lu\n", GetLastError());
+    }
+}
+BOOL APIENTRY DllMain(
+    HANDLE hModule,             // Handle to DLL module
+    DWORD ul_reason_for_call,   // Reason for calling function
+    LPVOID lpReserved           // Reserved
+) {
+
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH: // A process is loading the DLL.
+        // Do something here
+        break;
+    case DLL_THREAD_ATTACH: // A process is creating a new thread.
+        // Do something here
+        break;
+    case DLL_THREAD_DETACH: // A thread exits normally.
+        // Do something here
+        break;
+    case DLL_PROCESS_DETACH: // A process unloads the DLL.
+        // Do something here
+        break;
+    }
+    return TRUE;
+}
